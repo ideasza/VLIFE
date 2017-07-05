01dc4d8db2c966e8e9404c255990949426fbed05
ideasza <ozone_sk@hotmail.com>
2017-07-06 01:05:27 +0700

    -- Create menu layout, add SignUpActivity on 05/07/2017 01:00

--- app/src/main/java/teerayut/dev/vlife/main/MainActivity.java
+++ app/src/main/java/teerayut/dev/vlife/main/MainActivity.java
@@ -1,13 +1,26 @@
 package teerayut.dev.vlife.main;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import teerayut.dev.vlife.R;
 import teerayut.dev.vlife.base.BaseMvpActivity;
+import teerayut.dev.vlife.utils.ActivityResultBus;
+import teerayut.dev.vlife.utils.ActivityResultEvent;
 
 public class MainActivity extends BaseMvpActivity<MainInterface.Presenter> implements MainInterface.View {
+
+    private MenuItem menuItemClicked;
 
     @Override
     public MainInterface.Presenter createPresenter() {
@@ -19,6 +32,9 @@ public class MainActivity extends BaseMvpActivity<MainInterface.Presenter> imple
         return R.layout.activity_main;
     }
 
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.drawer) DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view) NavigationView navigationView;
     @Override
     public void bindView() {
         ButterKnife.bind(this);
@@ -31,7 +47,8 @@ public class MainActivity extends BaseMvpActivity<MainInterface.Presenter> imple
 
     @Override
     public void setupView() {
-
+        setToolbar();
+        setMainMenu();
     }
 
     @Override
@@ -47,5 +64,92 @@ public class MainActivity extends BaseMvpActivity<MainInterface.Presenter> imple
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ActivityResultBus.getInstance().postQueue(new ActivityResultEvent(requestCode, resultCode, data));
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setMainMenu() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked())
+                    menuItem.setChecked(false);
+                else
+                    menuItem.setChecked(true);
+
+                menuItemClicked = menuItem;
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+                if(menuItemClicked != null)
+                    handleSelectedMenu(menuItemClicked);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void handleSelectedMenu(MenuItem menuItem) {
+        //Check to see which item was being clicked and perform appropriate action
+        switch (menuItem.getItemId()){
+            //Replacing the main content with ContentFragment Which is our Inbox View;
+            case R.id.menu_home:
+                Toast.makeText(getApplicationContext(),"Inbox Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_profile:
+                Toast.makeText(getApplicationContext(),"Stared Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_register:
+                Toast.makeText(getApplicationContext(),"Send Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_news:
+                Toast.makeText(getApplicationContext(),"Drafts Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_product:
+                Toast.makeText(getApplicationContext(),"All Mail Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_purchase:
+                Toast.makeText(getApplicationContext(),"Trash Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(getApplicationContext(),"Spam Selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_logout :
+                break;
+            default:
+                Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
