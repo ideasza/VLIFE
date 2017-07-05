d39d788b389bfb1dd7d1a8fde985ea7b56de5f16
ideasza <ozone_sk@hotmail.com>
2017-07-05 08:54:52 +0700

    -- Add AuthenticationActivity on 05/06/2017

---
+++ app/src/main/java/teerayut/dev/vlife/authentication/AuthenticationActivity.java
@@ -0,0 +1,15 @@
+package teerayut.dev.vlife.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import teerayut.dev.vlife.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
