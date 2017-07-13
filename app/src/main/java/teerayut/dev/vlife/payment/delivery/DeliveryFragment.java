aef260bf38699cbc9ca451ef8ef366307679ece9
tsrmobile <teerayut.k@thiensurat.co.th>
2017-07-13 16:43:13 +0700

    -- Custom spinner province, district, sub-district  13/07/2017 16:45

--- app/src/main/java/teerayut/dev/vlife/payment/delivery/DeliveryFragment.java
+++ app/src/main/java/teerayut/dev/vlife/payment/delivery/DeliveryFragment.java
@@ -5,23 +5,53 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.reginald.editspinner.EditSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import teerayut.dev.vlife.R;
+import teerayut.dev.vlife.payment.delivery.item.ThailandItem;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DeliveryFragment extends Fragment {
-
+public class DeliveryFragment extends Fragment implements DeliveryInterface.View {
+    /**
+     * Insert form
+     */
+    private EditText editTextName;
+    private EditText editTextPhone;
+    private EditText editTextNumber;
+    private EditText editTextBuilding;
+    private EditText editTextVillage;
+    private EditText editTextSoi;
+    private EditText editTextRoad;
+    private EditText editTextZipcode;
+    private EditSpinner sub_districtSpinner;
+    private EditSpinner districtSpinner;
+    private EditSpinner provinceSpinner;
+    /**
+     * End
+     */
 
     public DeliveryFragment() {
         // Required empty public constructor
@@ -32,6 +62,7 @@ public class DeliveryFragment extends Fragment {
     }
 
     private View view;
+    private DeliveryInterface.Presenter presenter;
     private onClickButtonNext clickButtonNext;
     @BindView(R.id.group_delivery) RadioGroup groupDelivery;
     @BindView(R.id.group_address) RadioGroup groupAddress;
@@ -59,18 +90,21 @@ public class DeliveryFragment extends Fragment {
         radioAddress.setOnClickListener( OptionOnClickListener );
         radioAdd.setOnClickListener( OptionOnClickListener );
         buttonNext.setOnClickListener( onNext() );
+        setInstance();
     }
-
 
     RadioButton.OnClickListener OptionOnClickListener = new RadioButton.OnClickListener() {
         public void onClick(View v) {
             if (radioAddress.isChecked()) {
                 existAddress.setVisibility(View.VISIBLE);
                 insertAddress.setVisibility(View.GONE);
+                viewCurrentAddress();
             } else {
                 radioAddress.setChecked(false);
                 existAddress.setVisibility(View.GONE);
                 insertAddress.setVisibility(View.VISIBLE);
+                viewInsertAddress();
+                presenter.onLoadProvince();
             }
         }
     };
@@ -88,6 +122,10 @@ public class DeliveryFragment extends Fragment {
         }
     };
 
+    private void setInstance() {
+        presenter = new DeliveryPresenter(this);
+    }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -103,12 +141,77 @@ public class DeliveryFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.e("Test", districtSpinner.getText().toString());
                 clickButtonNext.OnClickButtonNext(view);
             }
         };
     }
 
+    @Override
+    public void setProvince(final List<ThailandItem> thailandItemList) {
+        ListAdapter adapter = new ArrayAdapter<ThailandItem>(getActivity(), android.R.layout.simple_spinner_dropdown_item, thailandItemList);
+        provinceSpinner.setAdapter(adapter);
+
+        provinceSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ThailandItem item = thailandItemList.get(position);
+                presenter.onLoadDistrict(item.getId());
+            }
+        });
+    }
+
+    @Override
+    public void setDistrict(final List<ThailandItem> thailandItemList) {
+        ListAdapter adapter = new ArrayAdapter<ThailandItem>(getActivity(), android.R.layout.simple_spinner_dropdown_item, thailandItemList);
+        districtSpinner.setAdapter(adapter);
+
+        districtSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ThailandItem item = thailandItemList.get(position);
+                presenter.onLoadSubDistrict(item.getId());
+            }
+        });
+    }
+
+    @Override
+    public void setSubDistrict(List<ThailandItem> thailandItemList) {
+        ListAdapter adapter = new ArrayAdapter<ThailandItem>(getActivity(), android.R.layout.simple_spinner_dropdown_item, thailandItemList);
+        sub_districtSpinner.setAdapter(adapter);
+
+        sub_districtSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
     public interface onClickButtonNext{
         void OnClickButtonNext(View view);
+    }
+
+    private TextView textViewSummary;
+    private void viewCurrentAddress() {
+        textViewSummary = (TextView) existAddress.findViewById(R.id.textViewSum);
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t" + "Name: " + "\n\n");
+        sb.append("\t" + "Phone: " + "\n\n");
+        sb.append("\t" + "Address: " + "\n\n");
+        sb.append("\t" + "Building: " + "\n\n");
+        sb.append("\t" + "Village:" + "\n\n");
+        sb.append("\t" + "Road: " + "\n\n");
+        sb.append("\t" + "Sub District: " + "\n\n");
+        sb.append("\t" + "District: " + "\n\n");
+        sb.append("\t" + "Province: " + "\n\n");
+        sb.append("\t" + "Zipcode: ");
+        textViewSummary.setText(sb.toString());
+    }
+
+    private void viewInsertAddress() {
+        provinceSpinner = (EditSpinner) insertAddress.findViewById(R.id.province_spinner);
+        districtSpinner = (EditSpinner) insertAddress.findViewById(R.id.district_spinner);
+        sub_districtSpinner = (EditSpinner) insertAddress.findViewById(R.id.sub_district_spinner);
     }
 }
